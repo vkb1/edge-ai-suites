edc51582a1b2e562adb8690ff3f63833bc9e5276
SudarshanaPanda <sudarshana.panda@intel.com>
2025-03-21 21:22:06 +0530

    Helm: Enabled Secured mode, mqtt and opc ua ingestion (#59)
    * Helm changes:
    1. Updated mqtt-publisher in helm charts
    2. Updated package_helm script with user guide changes
    3. docker-compose mqtt-publisher name correction
    
    
    ---------
    Signed-off-by: Sudarshana Panda <sudarshana.panda@intel.com>
    Signed-off-by: Vellaisamy, Sathyendran <sathyendran.vellaisamy@intel.com>
    Co-authored-by: Vinod Kumar B <vinod.k.b@intel.com>
    Co-authored-by: sathyendranv <84972945+sathyendranv@users.noreply.github.com>
    Co-authored-by: Vellaisamy, Sathyendran <sathyendran.vellaisamy@intel.com>

--- package_helm.sh
+++ package_helm.sh
@@ -1,13 +1,19 @@
 # Copyright Intel Corporation
-#!/bin/bash
+#!/bin/bash -e
 
 cp -f grafana/dashboards/*.json helm/
 cp -f grafana/dashboards/*.yml helm/
 cp -f influxdb/config/*.conf helm/
+cp -f influxdb/init-influxdb.sh helm/
 cp -f ../../tools/mqtt/broker/config/*.conf helm/
 cp -f telegraf/config/*.conf helm
-cp -f grafana/entrypoint.sh helm/
+cp -f grafana/entrypoint.sh helm/grafana_entrypoint.sh
 cp -f kapacitor/config.json helm/
-
+cp -f telegraf/entrypoint.sh helm/telegraf_entrypoint.sh
+cd ../../tools/k8s_secrets
+cp ../cert_gen.sh .
+cp ../san.cnf .
+docker build -f Dockerfile -t ia-cert-generator:1.0.0 .
+rm cert_gen.sh san.cnf
 
 
